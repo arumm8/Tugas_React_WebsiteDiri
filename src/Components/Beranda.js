66e9bcc6b41565cbb475529c8fb407ac4c933463
arumm8 <dewi_arum_30rpl@student.smktelkom-mlg.sch.id>
2023-03-06 11:17:25 +0700

    first commit

---
+++ src/Components/Beranda.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Beranda_1 } from './Beranda_1';
+import { Beranda_2 } from './Beranda_2';
+
+class Beranda extends React.Component{
+    render(){
+        return(
+            <>
+            <Beranda_1/>
+            <Beranda_2/>
+            </>
+        )
+    }
+}
+
+export default Beranda;
---
+++ src/Components/Gallery.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Gallery_1 } from './Gallery_1';
+
+class Gallery extends React.Component{
+    render(){
+        return(
+            <>
+            <Gallery_1/>
+            {/* <div>
+                <h2>Ini Tentang Saya</h2>
+                <h3>Yoww it's me!</h3>
+                <h4>Arum's Biodata : </h4>
+                <h5>Nama panjang : Arummmmm</h5>
+                <h5>Lengkapnya : Dewi Qhoni Sekar Arum</h5>
+                <h5>Tempat dan tanggal lahir : Originally Malang, 8 Januari 2006</h5>
+                <h6>Yoii baru ultah ke - 17 brodii</h6>
+                <h5>Sekolah : SMK Telkom Malang</h5>
+                <h6>Disini aku banyak belajar bersyukur. Gak gampang ke bawa arus. Dan yang pasti mandiri. Karena gak semua orang selalu ada buat kamu.</h6>
+                <h6>Disini banyak senengnya. Sedihnya? Lebih banyak lagi. Tetep dijalanin meskipun sambil hasbunallah wani'mal wakil yee kaan</h6>
+                <h5>Nah disini aku bakal ngenalin orang yang aku sayaangg bangett</h5>
+            </div> */}
+            </>
+        )
+    }
+}
+
+export default Gallery;
